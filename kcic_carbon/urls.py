78f171f45727f9d54515cd6cdfceed5ae2d10a4c
Elyne Cherotich <elynetooh@gmail.com>
2026-05-15 06:00:25 +0300

    add forgot password

--- kcic_carbon/urls.py
+++ kcic_carbon/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from footprint import views as footprint_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='footprint/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', footprint_views.register, name='register'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='footprint/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='footprint/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='footprint/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='footprint/password_reset_complete.html'), name='password_reset_complete'),
 ]

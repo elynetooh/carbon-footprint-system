0bdcf2a2945e313f3f4bd07af5efb1ef092d2e10
Elyne Cherotich <elynetooh@gmail.com>
2026-05-15 10:09:56 +0300

    Added logout route

--- footprint/urls.py
+++ footprint/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
     path('transport/', views.add_transport, name='add_transport'),
     path('energy/', views.add_energy, name='add_energy'),
     path('waste/', views.add_waste, name='add_waste'),
     path('water/', views.add_water, name='add_water'),
+
     path('download-pdf/', views.download_pdf, name='download_pdf'),
+
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('manager-dashboard/', views.manager_dashboard, name='manager_dashboard'),
+
     path('data-history/', views.data_history, name='data_history'),
+
+    # LOGOUT
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
